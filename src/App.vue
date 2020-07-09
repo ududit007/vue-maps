8357e393d25668ee264278c963e84161abf80190
ududit007 <ududit007@gmail.com>
2020-07-09 20:42:02 +0530

    added map script

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="#map" ref="map"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
+<style scoped>
+#map {
+  height: 600px;
+  background-color: gray;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
