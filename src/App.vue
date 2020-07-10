f86f7b2849189de0fe32fc2f926586ddaa034f43
ududit007 <ududit007@gmail.com>
2020-07-10 21:08:24 +0530

    fixed issue

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,30 @@
     </v-app-bar>
 
     <v-content>
-      <v-card class="ma-5">
+      <v-card class="ma-5" flat>
       <v-container>
     <v-row justify="center">
-     <v-card>
-        <v-text-field placeholder="enter address" v-model="address" />
-        <v-card-actions class="justify-center">
-        <v-btn class="primary" justify="center" @click="getLocation">submit</v-btn>
-      </v-card-actions>
-      </v-card>
+     <!-- <v-card> -->
+      <!-- <div> -->
+        <v-col xs="2" />
+        <v-col xs="4">
+        <v-text-field placeholder="enter address" prepend-icon="mdi-map-marker" v-model="address" />
+      </v-col>
+      <v-col xs="4">
+         <v-btn icon>
+        <v-icon @click="getLocation">mdi-magnify</v-icon>
+      </v-btn>
+    </v-col>
       <br/>
     </v-row>
     <br>
     <!-- <v-row justify="center"> -->
-      <div id="map" ref="map">
+      <div id="map" ref="map" v-if="show">
       </div>
-      <p>{{address}} {{ lat }} {{ lng}}</p>
+      <v-alert v-if="lng==0 && lat==0" type="error" dismissible  dense top elevation="2">
+      your address is invalid
+    </v-alert>
+
        <!-- </v-row> -->
         </v-container>
       </v-card>
@@ -43,40 +51,27 @@ export default {
       map: null,
       lat: -25.344,
       lng: 131.036,
-      latlng: {lat: -25.344, lng: 131.036}
+      show: false
     }),
     methods: {
-      getLocation() {
+      async getLocation() {
         const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + this.address + '.json?access_token=pk.eyJ1IjoidWR1ZGl0MDA3IiwiYSI6ImNrMWhpejZ4NDFhaDAzaHFtdndpejZlMXoifQ.B_k4N8sXB5F-HyK1sXi8qw'
-        this.$http.get(url).then(response=>{
+        await this.$http.get(url).then(response=>{
         if (response.body.features.length > 0){
                     this.lat=response.body.features[0].center[1];
                     this.lng=response.body.features[0].center[0];
+                    this.show = true;
                   }
                   else {
-                    alert("entered address doesn't exist")
+                    this.show = false;
+                    this.lat = 0;
+                    this.lng = 0;
                   }
         },error=>{
           console.log("error", error)
         })
 
-
-        
-      }
-    },
-    updated() {
-      this.map = new window.google.maps.Map(this.$refs["map"], {
-        center: {lat: this.lat, lng: this.lng},
-        zoom: 7
-      })
-      new window.google.maps.Marker({
-        position: {lat: this.lat, lng: this.lng},
-        map:  this.map
-      })
-     
-    },
-    mounted() {
-      this.map = new window.google.maps.Map(this.$refs["map"], {
+        this.map = new window.google.maps.Map(this.$refs["map"], {
         center: {lat: this.lat, lng: this.lng}, 
         zoom: 7
       })
@@ -84,6 +79,19 @@ export default {
         position: {lat: this.lat, lng: this.lng},
         map:  this.map
       })
+
+        
+      }
+    },
+    created() {
+      
+    },
+    updated() {
+     
+    },
+    
+    mounted() {
+      
     }
   }
 </script>
